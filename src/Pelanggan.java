e7a9df2178fa1cdb4db575293242127da827e660
Fikri-Codes <fikrianwar9345@gmail.com>
2025-02-28 10:34:06 +0700

    Edit : pelanggan.java

---
+++ src/Pelanggan.java
@@ -0,0 +1,33 @@
+package src;
+public class Pelanggan {
+  private String nama_pelanggan;
+  private String tanggal_belanja;
+  private boolean member;
+  private String id_member;
+  
+  public void setNama (String a) {
+    this.nama_pelanggan = a;
+  }
+  public void setTanggalBelanja (String a) {
+    this.tanggal_belanja=a;
+  }
+  public void setMember (int b) {
+    if (b==1) this.member=true;
+    else this.member=false;
+  }
+  public void setIdMember (String b) {
+    if (this.member) this.id_member=b;
+  }
+
+  public void tampilkanInfo () {
+    System.out.println("Nama Pelanggan : " + this.nama_pelanggan);
+    System.out.println("Tanggal Belanja : " + this.tanggal_belanja);
+    System.out.print("Member : ");
+    if (member) {
+      System.out.println("Iya");
+      System.out.println("Id Member : " + this.id_member);
+    } else System.out.println("Tidak");
+    System.out.println("===============");
+  }
+
+}
